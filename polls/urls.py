0f8fa2ad3633f5e0c020c4fe3279a0a99e0db818
morachake <jacobmoracha@gmail.com>
2025-01-15 19:22:09 +0300

    Add URL patterns for polls list and detail views

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,13 @@
+def polls_list(request):
+    pass
+
+def polls_detail(request, pk):
+    pass
+
+from django.urls import path
+from .views import polls_list, polls_detail
+
 urlpatterns =[
-    
+    path('polls/', polls_list, name='polls_list'),
+    path('polls/<int:pk>/', polls_detail, name='polls_detail'),
 ]
